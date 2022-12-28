5c68d810496eb9f7d017aa7d83f2250894fedf5c
Shyam Bharati <sunny8r0@gmail.com>
2022-12-28 17:41:15 +0530

    intial commit

---
+++ app/src/main/java/com/example/differentlayouts/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.differentlayouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.constraint_layout);
+        button2 = (Button) findViewById(R.id.linear_layout);
+        button3 = (Button) findViewById(R.id.relative_layout);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),ConstraintLayout.class);
+                startActivity(i);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),LinearLayout.class);
+                startActivity(i);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =  new Intent(getApplicationContext(),RelativeLayout.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
