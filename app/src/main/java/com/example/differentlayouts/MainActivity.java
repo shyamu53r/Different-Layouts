8a302fd10b050a0ff818c9387cfc5cb609f2670d
Shyam Bharati <sunny8r0@gmail.com>
2022-12-29 11:17:44 +0530

    modified files

--- app/src/main/java/com/example/differentlayouts/MainActivity.java
+++ app/src/main/java/com/example/differentlayouts/MainActivity.java
@@ -12,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
     private Button button1;
     private Button button2;
     private Button button3;
-    private Button button4;
-    private Button button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
